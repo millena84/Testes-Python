87fdb382df948caed6c99b942aa9367a72bada13
Millena Ferreira dos Reis <millena.ferreira@gmail.com>
2017-06-18 17:38:40 -0300

    Add files via upload

---
+++ Tkinter-t1.py
@@ -0,0 +1,7 @@
+  class Application:
+  	import Tkinter
+      def __init__(self, master=None):
+          pass
+  root = Tk()
+  Application(root)
+  root.mainloop()
---
+++ Tkinter-t2.py
@@ -0,0 +1,10 @@
+import Tkinter
+
+class Application:
+	def __init__(self, master=None):
+		pass
+
+root = Tkinter.Tk()
+Application(root)
+root.mainloop()
+
